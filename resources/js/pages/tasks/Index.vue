6fed6079225b1b5323efaddb71489275865a56ac
mshadiqaf <mshadiqaf@gmail.com>
2025-06-29 01:08:28 +0800

    First initialization with authentication and dashboard

---
+++ resources/js/pages/tasks/Index.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+import Heading from '@/components/Heading.vue';
+import AppLayout from '@/layouts/AppLayout.vue';
+import { type BreadcrumbItem } from '@/types';
+import { Head } from '@inertiajs/vue3';
+
+const breadcrumbs: BreadcrumbItem[] = [
+    {
+        title: 'Tasks',
+        href: '/tasks',
+    },
+];
+</script>
+
+<template>
+    <Head title="Task Lists" />
+
+    <AppLayout :breadcrumbs="breadcrumbs">
+        <div class="px-4 py-6">
+            <Heading title="Tasks" description="Manage your to-do list tasks"/>
+
+            <div class="flex flex-col space-y-8 md:space-y-0 lg:flex-row lg:space-y-0 lg:space-x-12">
+                
+            </div>
+        </div>
+    </AppLayout>
+</template>
---
+++ routes/web.php
@@ -0,0 +1,20 @@
+<?php
+
+use App\Http\Controllers\TaskController;
+use Illuminate\Support\Facades\Route;
+use Inertia\Inertia;
+
+Route::get('/', function () {
+    return Inertia::render('Welcome');
+})->name('home');
+
+Route::get('dashboard', function () {
+    return Inertia::render('Dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware(['auth', 'verified'])->group( function() {
+    Route::get('/tasks', [TaskController::class, 'index'])->name('tasks.index');
+});
+
+require __DIR__.'/settings.php';
+require __DIR__.'/auth.php';
